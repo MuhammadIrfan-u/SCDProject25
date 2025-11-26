20a754bfb0ac6a16318cdf54e10d4f8bb1c1b2ad
MuhammadIrfan-u <irfanjunego332211@gmaol.com>
2025-11-26 10:16:49 +0000

    Dockerfile created for the Backend

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ require('dotenv').config(); // Load environment variables
 const db = require('./db');
 require('./events/logger'); // Initialize event logger
 
+mongoose.set('strictQuery', true);
+
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
